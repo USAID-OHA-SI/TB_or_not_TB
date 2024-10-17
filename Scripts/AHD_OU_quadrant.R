e9f904c58e46a1c386ee4e14e6dc7df6ca27476d
Karishma Srikanth <84093378+karishmas26@users.noreply.github.com>
2024-10-17 15:47:02 -0400

    add age filter, remove agency filter, and change to OU

--- Scripts/AHD_OU_quadrant.R
+++ Scripts/AHD_OU_quadrant.R
@@ -42,10 +42,11 @@ metadata <- get_metadata(path)
 #FY24Q3 TX_NEW  
 df_cd4 <- df_msd %>% 
     filter(indicator == "TX_NEW",
-           funding_agency == "USAID",
+          # funding_agency == "USAID",
            fiscal_year == metadata$curr_fy,
+          age_2019 %ni% c("01-04", "<01"),
            standardizeddisaggregate == "Age/Sex/CD4/HIVStatus") %>% 
-    group_by(fiscal_year, indicator, country,
+    group_by(fiscal_year, indicator, operatingunit,
              otherdisaggregate) %>% 
     summarise(across(starts_with("cumulative"), sum, na.rm = T), .groups = "drop") %>% 
     pivot_wider(names_from = "otherdisaggregate", values_from = "cumulative") %>% 
@@ -56,12 +57,12 @@ df_cd4 <- df_msd %>%
     mutate(total = `<200 CD4`+`>=200 CD4`+`CD4 Unknown`,
            pct_completeness = (`<200 CD4`+`>=200 CD4`) / total,
            pct_ahd = (`<200 CD4`)/(`<200 CD4`+`>=200 CD4`)) %>% 
-    mutate(country = case_when(country == "Democratic Republic of the Congo" ~ "DRC",
-                                     country == "Dominican Republic" ~ "DR",
-                                     TRUE ~ country)) %>% 
-    filter(country != "Jamaica") #fltering out due to data quality
-    
-    
+    mutate(operatingunit = case_when(operatingunit == "Democratic Republic of the Congo" ~ "DRC",
+                                     operatingunit == "Dominican Republic" ~ "DR",
+                                     TRUE ~ operatingunit)) %>% 
+    mutate(pct_ahd = ifelse(`<200 CD4` == 0 & `>=200 CD4` == 0, 0, pct_ahd)) %>% 
+    filter(operatingunit != "South Sudan")
+
  
   
   # VIZ --------------------------------------------------------------------------
@@ -74,22 +75,24 @@ df_cd4 <- df_msd %>%
     geom_point(size=3) +
     geom_hline(yintercept = .2, colour = "#D3D3D3") +
     geom_vline(xintercept = .50, colour = "#D3D3D3") +
-    scale_x_continuous(labels = scales::percent) +
-    scale_y_continuous(labels = scales::percent) +
+    scale_x_continuous(labels = scales::percent, limits = c(0,1)) +
+    scale_y_continuous(labels = scales::percent, limits = c(0,.6)) +
+    #c(2004, seq(1990, 2020, by = 10))
     scale_color_identity() +
-    ggrepel::geom_text_repel(aes(label = country), size = 4, na.rm = TRUE, family = "Source Sans Pro") +
+    ggrepel::geom_text_repel(aes(label = operatingunit), size = 4, na.rm = TRUE, family = "Source Sans Pro",
+                             max.overlaps = 60) +
     si_style_nolines() +
     labs(x = "Reported CD4 Coverage",
          y = "Percent AHD",
          title = "CD4 Reporting Completeness and Advanced HIV Disease Rates for Patients Newly initiated on treatment" %>% toupper(),
         # title = "<span style= 'color:#5BB5D5;'>CD4 COVERAGE GAPS</span> STILL EXIST - EVEN WHERE <span style= 'color:#15478A;'>CD4 COVERAGE IS HIGH, MANY COUNTRIES HAVE HIGH AHD PREVALENCE</span>",
-         subtitle = "FY24Q3 cumulative reported results at USAID-supported sites",
+         subtitle = "FY24Q3 cumulative reported results at PEPFAR-supported sites",
          caption = glue("Source: {metadata$source} | Ref id: {ref_id}
-                        Note: Countries without reported CD4 testing in the time period ommitted from plot")) +
+                        Note: Results for <5 ommitted from plot")) +
     theme(plot.title = element_markdown())
   
-  si_save("Graphics/AHD_by_country.svg")
-  si_save("Images/AHD_by_country.png")
+  si_save("Graphics/AHD_by_OU_PEPFAR.svg")
+  si_save("Images/AHD_by_OU_PEPFAR.png")
   
   
   #CD4 coverage gaps still exist and 2) Even where coverage is high, a large proportion of patients are still presenting with AHD
